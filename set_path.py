94f9c0d993e6478adae9d02d3bdb01b7f74c6d13
Shanshan Gao <gao33.bnl@gmail.com>
2018-03-05 16:08:52 -0500

    update
    for mac user

--- set_path.py
+++ set_path.py
@@ -5,7 +5,7 @@ Author: GSS
 Mail: gao.hillhill@gmail.com
 Description: 
 Created Time: Sun Feb 25 20:58:38 2018
-Last modified: Sun Feb 25 21:05:47 2018
+Last modified: Mon Mar  5 16:08:32 2018
 """
 
 ###############################################################################
@@ -14,7 +14,7 @@ def set_path(os="mac"):
     if os == "linux" :
         rawpath = "/nfs/rscratch/bnl_ce/shanshan/Rawdata/" 
     elif os == "mac" :
-        rawpath = "~/ProtoDUNE/Rawdata/" 
+        rawpath = "./ProtoDUNE/Rawdata/" 
     elif os == "windows" :
         rawpath = "D:/APA/Rawdata/" 
     else:
