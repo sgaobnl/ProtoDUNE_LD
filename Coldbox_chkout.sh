38d9b1fa4e34ec32355419c5de60496db294f6b2
Shanshan Gao <gao33.bnl@gmail.com>
2018-03-19 13:40:22 -0400

    add script for cold box test

---
+++ Coldbox_chkout.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python Main.py Coldbox 0x00 True mac
+python Main.py Coldbox 0x10 True mac
+python Main.py Coldbox 0x80 True mac
+
---
+++ Coldbox_full_chk.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python Main.py Coldbox 0x00 True mac
+python Main.py Coldbox 0x17 True mac
+python Main.py Coldbox 0x80 True mac
+
