4690933f57660033c6def55a94a55e60a8c7629d
sgaobnl <gao33.bnl@gmail.com>
2018-10-16 14:49:37 -0400

    update

--- MainCOTS.py
+++ MainCOTS.py
@@ -5,7 +5,7 @@ Author: GSS
 Mail: gao.hillhill@gmail.com
 Description: 
 Created Time: 1/13/2018 3:05:03 PM
-Last modified: 10/16/2018 10:47:53 AM
+Last modified: 10/16/2018 2:48:26 PM
 """
 
 #defaut setting for scientific caculation
@@ -62,7 +62,7 @@ elif (ceruns.APA == "LArIAT"):
     #ceruns.path = "D:/APA40/Rawdata/" 
     #ceruns.path = "D:/APA40/Rawdata/" 
     #ceruns.path = "/Users/shanshangao/LArIAT/Rawdata/" 
-    ceruns.path = "D:/APA7_ICEBERG/Screen_rawdata/" 
+    ceruns.path = "D:/Ledge_Study/Rawdata/" 
     ceruns.wib_ips = [  "192.168.121.1"  ]
     #ceruns.wib_ips = [  "131.225.150.203",  "131.225.150.206" ]
     ceruns.wib_pwr_femb = [[1,0,0,0], [0,0,0,0]]
@@ -248,7 +248,7 @@ if (test_runs&0x10 != 0x0 ):
 if (test_runs&0x01 != 0x0 ):
     print "Noise Measurement Test"
     print "time cost = %.3f seconds"%(timer()-start)
-    ceruns.rms_run(apa_oft_info, sgs = [1,3], tps =[0,1,2,3], val=1600) 
+    ceruns.rms_run(apa_oft_info, sgs = [0, 2, 1,3], tps =[0,1,2,3], val=1600) 
     with open(logfile, "a+") as f:
         f.write( "%2X: Noise Measurement Test\n" %(test_runs&0x01) ) 
         f.write (ceruns.runpath + "\n" )
@@ -258,7 +258,7 @@ if (test_runs&0x01 != 0x0 ):
 if (test_runs&0x02 != 0x0 ):
     print "FPGA DAC Calibration Test"
     print "time cost = %.3f seconds"%(timer()-start)
-    ceruns.fpgadac_run(apa_oft_info, sgs = [1,3], tps =[0,1,2,3], val=100)
+    ceruns.fpgadac_run(apa_oft_info, sgs = [0,2, 1,3], tps =[0,1,2,3], val=100)
     with open(logfile, "a+") as f:
         f.write( "%2X: FPGA DAC Calibration Test\n" %(test_runs&0x02) ) 
         f.write (ceruns.runpath + "\n" )
